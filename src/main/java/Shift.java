6527c52d8790456c72f31c348d4511174d8ac814
narain karthikeyan <NarainKarthikeyan64>
2016-02-05 16:20:08 -0500

    refactored

--- src/main/java/Shift.java
+++ src/main/java/Shift.java
@@ -5,6 +5,8 @@ public class Shift {
 int startTimeHours;
 int bedTimeHours;
 
+final Double BEGINNING_HOUR_RATE = 12.00;
+
     public Shift(int beginHours, int endHours) {
         if (beginHours >= 17 && (endHours <= 4 || endHours >= 17)) {
             startTimeHours = beginHours;
@@ -21,8 +23,8 @@ int bedTimeHours;
     }
 
     public Double calculatePay() {
-        return 12.00 * (double)(20 - startTimeHours);
-    } 
+        return BEGINNING_HOUR_RATE * (double)(20 - startTimeHours);
+    }
 
 
 
